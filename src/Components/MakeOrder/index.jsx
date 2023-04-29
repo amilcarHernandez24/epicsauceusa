5b34c1dbfa4e13716086f1bb628fa52ffd84a71f
Amilcar <amilcarjh24@gmail.com>
2023-04-29 14:51:44 -0300

    add new images to the MakeOrder section and CSS

--- src/Components/MakeOrder/index.jsx
+++ src/Components/MakeOrder/index.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import styles from './makeOrder.module.css';
+import epicImg from '../../assets/logo2.png';
+import cilantroImg from '../../assets/cilantro.png';
 
 export default function MakeOrder() {
     return (
-        <div className={styles.saludo}>
-            <h4>Hola</h4>
+        <div className={styles.order_box}>
+            <section className={styles.header}>
+                <h2 className={styles.title}>Make Your Order</h2>
+                <p>Made with natural ingredients an a touch of love, we take care of every detail of the process to guarantee you a high quality product</p>
+            </section>
+            <section className={styles.content}>
+                <div className={styles.left_content}>
+                    <h3>Phone: + 1 123-456-7890</h3>
+                    <h3>Email: testemail@email.com</h3>
+                    <img src={epicImg} alt="epic sauce logo" className={styles.logo_img}/>
+                    <img src={cilantroImg} alt="cilantro image" className={styles.cilantro_img}/>
+                </div>
+                <div>
+                    <h4>Formulario</h4>
+                </div>
+            </section>
         </div>
     )
 }
